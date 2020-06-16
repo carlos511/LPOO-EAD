dadc8f77c3299ebffd4f3c718d0db43153033cb3
carlos511 <38634751+carlos511@users.noreply.github.com>
2020-06-16 13:20:50 -0300

    Add files via upload

---
+++ lista2ex10/Lista2EX10.java
@@ -0,0 +1,24 @@
+
+package lista2ex10;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Lista2EX10 {
+
+    public static void main(String[] args) {
+             naturais(5);
+    }
+   public static void naturais(int n){
+  if (n>0){   
+   if(n>0){    
+    System.out.println(n);
+    naturais(n-1);
+   }  
+ }
+   
+ }
+}
+    
+
