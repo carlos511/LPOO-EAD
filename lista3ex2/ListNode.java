ba8e2a854b2589ba52bb65d289e84d5dafedac56
carlos511 <38634751+carlos511@users.noreply.github.com>
2020-06-16 13:21:46 -0300

    Add files via upload

---
+++ lista3ex2/ListNode.java
@@ -0,0 +1,16 @@
+
+package lista3ex2;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ListNode {
+     int val;
+    ListNode next;
+
+    public ListNode(int v, ListNode n){ 
+            val = v;
+            next = n;
+    }
+}
---
+++ lista3ex2/Lista3EX2.java
@@ -0,0 +1,59 @@
+
+package lista3ex2;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Lista3EX2 {
+
+    public static void main(String[] args) {
+     LinkedIntList l1 = new LinkedIntList();
+        LinkedIntList l2 = new LinkedIntList();
+       
+        for(int i = 10; i>0 ; i--){
+            l1.addFirst(i);  
+        }
+        for(int i = 0; i<10 ; i++){
+            l2.addFirst(i);  
+        }
+        
+        l2.interList(l1, l2);
+        
+        l2.print();
+        
+        
+         
+        //Copiar Lista
+        l2.copList(l1, l2);        
+        l2.print();        
+        System.out.println("");        
+        l1.print();
+        
+        
+        
+        
+        for(int i = 10; i>0 ; i--){
+            l2.addFirst(i);  
+        }
+        // Concatenar Lista
+         l1.concList(l2);
+         
+         //Imprimir determinado elemento da Lista
+         for(int i = l1.size()-1; i>0; i--){
+            int c = l1.get(i);
+          if(c == 20){
+                System.out.println(l1.get(i));
+          }
+        }
+        System.out.println(l1.size());
+        
+        
+        l1.print();
+        System.out.println("");
+        
+        
+        System.out.println(l1.size());
+    }
+    
+}
